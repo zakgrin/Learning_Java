03ce5664992f580e8e5bded338c346a3c0ef10af
zakgrin <zakariya.abugrin@gmail.com>
2020-11-01 21:35:13 +0100

    add print and variables

---
+++ 02_Variables/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        // Variables:
+        int age = 29;
+        double weight = 83.4; 
+        boolean isMoving = true; 
+        char size = 'L';
+        System.out.println(size); 
+
+        /*
+        * Multiple line comments
+        */ 
+    }
+}
